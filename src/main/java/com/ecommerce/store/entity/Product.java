813c4ba34fa4789bbf218c57ae1a38d3f70b4be7
Junior <eecfredes@gmail.com>
2026-02-15 22:35:20 -0300

    :rocket:Product entity

---
+++ src/main/java/com/ecommerce/store/entity/Product.java
@@ -0,0 +1,28 @@
+package com.ecommerce.store.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private Double price;
+
+    private Integer stock;
+}
