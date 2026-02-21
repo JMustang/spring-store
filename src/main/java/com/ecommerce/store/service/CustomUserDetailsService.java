fafa91cccb9120671e2a048b7ca2b52d0b28e720
Junior <eecfredes@gmail.com>
2026-02-20 21:09:33 -0300

    :package:Created a custom User details service

---
+++ src/main/java/com/ecommerce/store/service/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.ecommerce.store.service;
+
+import com.ecommerce.store.entity.User;
+import com.ecommerce.store.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        User user = repository.findAll()
+                .stream()
+                .filter(u -> u.getEmail().equals(email))
+                .findFirst()
+                .orElseThrow(() -> new UsernameNotFoundException("❌ User not found"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                Collections.singleton(new SimpleGrantedAuthority(user.getRole()))
+        );
+    }
+}
