073a80ce177681ee7e5024ef22bbb28e320a69c4
Junior <eecfredes@gmail.com>
2026-02-23 22:34:42 -0300

    :package:Security config adjustment

--- src/main/java/com/ecommerce/store/config/SecurityConfig.java
+++ src/main/java/com/ecommerce/store/config/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig {
 
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login", "/css/**").permitAll()
+                        .requestMatchers("/login", "/register", "/css/**").permitAll()
                         .anyRequest().authenticated()
 
                 )
