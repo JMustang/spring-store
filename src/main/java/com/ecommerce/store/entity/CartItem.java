b9544f1de92127348789576facbcdd18cc842d87
Junior <eecfredes@gmail.com>
2026-02-28 21:51:01 -0300

    :package:Creating a CartItem Entity.

---
+++ src/main/java/com/ecommerce/store/entity/CartItem.java
@@ -0,0 +1,28 @@
+package com.ecommerce.store.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "cart_itens")
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+}
